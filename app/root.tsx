ac4e621ffb37e2d77a30d50b101a8e5d890e2aec
Peter Pham <pttp15122002@gmail.com>
2026-02-15 17:06:41 +1100

    style: remove fixed width

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="max-w-7xl mx-auto text-black">
+      <body className="text-black">
         <Header />
         {children}
         <ScrollRestoration />
