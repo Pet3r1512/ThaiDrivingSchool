8f410e1a0cb25e5072e95dc13fe7bbfd978b8987
Peter Pham <pttp15122002@gmail.com>
2026-02-15 17:06:56 +1100

    imp: add customized size attribute

--- app/components/layout/Header/BookNowBtn.tsx
+++ app/components/layout/Header/BookNowBtn.tsx
@@ -1,5 +1,9 @@
 import { Button } from "~/components/ui/Button";
 
-export default function BookNowBtn() {
-  return <Button size="sm">Book a Lesson</Button>;
+export default function BookNowBtn({
+  size = "sm",
+}: {
+  size?: "sm" | "md" | "lg" | undefined;
+}) {
+  return <Button size={size}>Book a Lesson</Button>;
 }
