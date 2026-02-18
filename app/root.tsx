6376adf9b13edaec2ea6e7ee6f2e0003e9656a22
Peter Pham <pttp15122002@gmail.com>
2026-02-18 22:50:06 +1100

    chore: error page

--- app/root.tsx
+++ app/root.tsx
@@ -55,7 +55,7 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   let stack: string | undefined;
 
   if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
+    message = error.status === 404 ? "404 - Page not Found" : "Error";
     details =
       error.status === 404
         ? "The requested page could not be found."
@@ -66,8 +66,10 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
+    <main className="px-5 py-12 md:py-24 lg:py-32 container mx-auto min-h-[75dvh] flex flex-col items-center justify-center">
+      <h1 className="text-lg md:text-xl lg:text-2xl font-bold text-amber-500">
+        {message}
+      </h1>
       <p>{details}</p>
       {stack && (
         <pre className="w-full p-4 overflow-x-auto">
